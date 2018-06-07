4209f0346dde5514545f4a487135279445bb9e6f
Prithvi Pal Singh <Prithvi.Coer@outlook.com>
2018-06-07 14:40:49 +0800

    Add project files.

---
+++ SubStringSearch/BruteForceWay.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubStringSearch
+{
+    class BruteForceWay
+    {
+        public int FirstSearchImplementation(string text, string pattern)
+        {
+            int N = text.Length;
+            int M = pattern.Length;
+
+            for (int i = 0; i < N - M; i++)
+            {
+                int j;
+                for (j = 0; j < M; j++)
+                {
+                    if (text[i + j] != pattern[j])
+                        break;
+                }
+
+                if (j == M)
+                    return i;
+            }
+
+            return N;
+        }
+
+        public int SecondSearchImplentation(string text, string pattern)
+        {
+            int N = text.Length;
+            int M = pattern.Length;
+            int i, j;
+
+            for (i = 0, j = 0; i < N - M; i++)
+            {
+                if (text[i] == pattern[j])
+                {
+                    j++;
+                }
+                else
+                {
+                    i -= j;
+                    j = 0;
+                }
+            }
+
+            if (j == M)
+            {
+                return i - M;
+            }
+
+            return N;
+        }
+    }
+}
---
+++ SubStringSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubStringSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BruteForceWay bfw = new BruteForceWay();
+            int startIndex = bfw.FirstSearchImplementation("AABBCCDD", "CCD");
+            if (startIndex == "AABBCCDD".Length)
+                Console.WriteLine("Not found");
+            else
+                Console.WriteLine($"Start Index: {startIndex} <<>> End Index: {startIndex + "CCD".Length}");
+
+            int stIndex = bfw.FirstSearchImplementation("AABBCCDD", "CCD");
+            if (stIndex == "AABBCCDD".Length)
+                Console.WriteLine("Not found");
+            else
+                Console.WriteLine($"Start Index: {stIndex} <<>> End Index: {startIndex + "CCD".Length}");
+
+            Console.Read();
+        }
+    }
+}
